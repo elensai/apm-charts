6467cc099b580e93ef5630e34c963851814c8561
xiaobao <luxinyue@elensdata.com>
2020-03-18 17:20:35 +0800

    fix

--- _update.sh
+++ _update.sh
@@ -7,7 +7,6 @@ if [ -z "$1" ];then
   echo "default commit"
 fi
 
-
 list=`find . -name 'apm-*' -type d ! -empty | awk -F/ '{print $2}'`
 # echo "$list" > list.md
 
@@ -28,46 +27,6 @@ do
   echo "\n"
 done < list.md
 
-# for i in $list
-# do
-#   echo "$i";
-#   curl -XDELETE http://192.168.1.244:22317/api/charts/$i/0.1.0
-#   echo "\n"
-# done;
-
-# for i in $list
-# do
-#   helm dep update $i
-#   helm package $i
-#   curl --data-binary "@${i}-0.1.0.tgz" http://192.168.1.244:22317/api/charts
-# done;
-
 rm -f apm*tgz
 
-git add . && git commit -m "$commit" && git push origin master
-
-# ------
-
-# git add .; git commit -m 'fix'; 
-
-# helm package .
-
-# curl -XDELETE http://192.168.1.244:22317/api/charts/mysql57/0.1.0
-
-# curl --data-binary "@mysql57-0.1.0.tgz" http://192.168.1.244:22317/api/charts
-
-# cd ../test-nginx-chart
-
-# rm -f charts/mysql57-0.1.0.tgz
-
-# helm dependency update 
-
-# version='version: '`date | awk '{print $4}'| sed -n 's/:/./pg'`
-
-# git add .; git commit -m 'fix';
-
-# cd ../test-charts
-
-# git subtree pull --prefix=test-nginx-chart sub-nginx master --squash
-
-# git push origin master
+# git add . && git commit -m "$commit" && git push origin master
